4463e94440d9c3c7f984769b776bf45556813d9b
John Robinson <github@torrlane.co.uk>
2017-04-18 23:27:57 +0100

    use let syntax in unit tests to make them more readable

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,23 +12,52 @@ import qualified AltLib as A (Dividend(..), Transaction(..), Holding(..), divide
 import ParseDividends
 
 
+{-
+ - test dividends_paid_upto returns 0 when no dividends are supplied
+ -}
 test_empty_dividend_calculation :: Assertion
-test_empty_dividend_calculation = assertEqual "fail dividend" 0 $ A.dividends_paid_upto epoch [] []
+test_empty_dividend_calculation = assertEqual "" 0 $ A.dividends_paid_upto undefined [] []
 
-test_dividend_calculation = assertEqual "fail dividend" 80 $ A.dividends_paid_upto epoch [A.Dividend{A.paid_on=epoch, A.amount=10}] [A.Transaction{A.transaction_date=epoch,A.shares_bought=8,A.cost=8}]
+test_dividend_calculation = 
+    let purchaseDate = epoch
+        dividendPaymentDate = epoch
+        transaction = A.Transaction{A.transaction_date=purchaseDate,A.shares_bought=8,A.cost=0}
+        dividend = A.Dividend{A.paid_on=dividendPaymentDate, A.amount=10} 
+        expected = 80 in
+    assertEqual "" expected $ A.dividends_paid_upto epoch [dividend] [transaction]
 
+test_parseHolding = 
+    let expected = A.Holding{A.share="TSCO", A.transactions=[], A.dividends=[]} in
+    assertEqual "" expected $ A.parseHolding "Holding{share=\"TSCO\", transactions=[], dividends=[]}"
 
-test_parseHolding = assertEqual "failed to parseLine " A.Holding{A.share="TSCO", A.transactions=[], A.dividends=[]} $ A.parseHolding "Holding{share=\"TSCO\", transactions=[], dividends=[]}"
+{-
+ - Test that getShareName successfully extracts the share name from the csv line
+ -}
+test_getShareName_from_csvLine_1 = 
+    let expected = "Henderson International Income Trust plc" 
+        csvLine = "Income history for:, " ++ expected ++ ", Ord GBP0.01 , , ," in
+    assertEqual "" Just expected $ getShareName csvLine
 
-test_get_share1 = assertEqual "unlucky!" (Just "Henderson International Income Trust plc") $ getShareName "Income history for:, Henderson International Income Trust plc, Ord GBP0.01 , , ,"
+test_getShareName_from_csvLine_2 = 
+    let expected = "Royal Dutch Shell Plc A Shares"
+        csvLine = "Income history for:, " ++ expected ++ ", EUR0.07 , , ," in
+    assertEqual "" Just expected $ getShareName csvLine
 
-test_get_share2 = assertEqual "unlucky!" (Just "Royal Dutch Shell Plc A Shares") $ getShareName "Income history for:, Royal Dutch Shell Plc A Shares, EUR0.07 , , ,"
+test_getShareName_from_csvLine_3 = 
+    let expected = "Biotech Growth Trust (The)"
+        csvLine = "Security movements for:, " ++ expected ++ ", Ordinary 25p , , ," in
+    assertEqual "" Just expected $ getShareName csvLine
 
-test_get_share3 = assertEqual "" (Just "Biotech Growth Trust (The)") $ getShareName "Security movements for:, Biotech Growth Trust (The), Ordinary 25p , , ,"
+test_parse_transaction = 
+    let expectedTransactionDate = fromGregorian 2016 12 07
+        csvTransactionDate = "07/12/2016"
+        expected = A.Transaction {A.transaction_date=expectedTransactionDate, A.shares_bought=1, A.cost=11} in
+    assertEqual "" expected $ runParseRecordTest [csvTransactionDate, "_", "_", "_", "1.00", "11"] 
 
-test_parse_transaction = assertEqual "" A.Transaction {A.transaction_date=fromGregorian 2016 12 07, A.shares_bought=1, A.cost=11} $ runParseRecordTest ["07/12/2016", "_", "_", "_", "1.00", "11"] 
-
-test_parse_dividend = assertEqual "" A.Dividend{A.paid_on=fromGregorian 2016 12 07, A.amount=28.9} $ runParseRecordTest ["07/12/2016","ST DIV","share name","n/a","80.00","23.12"]
+test_parse_dividend = 
+    let expected = A.Dividend{A.paid_on=fromGregorian 2016 12 07, A.amount=28.9}
+        csvLine = ["07/12/2016","ST DIV","share name","n/a","80.00","23.12"] in
+    assertEqual "" expected $ runParseRecordTest csvLine
 
 {-
  - Take a "csv" as input and parse it into a type using the Data.Csv.parseRecord method, which will, in turn, use the appropriate fromRecord function for the desired type.
@@ -44,12 +73,12 @@ main :: IO ()
 main = defaultMain
         [testCase "test1" test1, 
         testCase "test2" test2, 
-        testCase "test4" test_empty_dividend_calculation, 
-        testCase "test5" test_dividend_calculation,
-        testCase "test6" test_parseHolding,
-        testCase "get_share1" test_get_share1,
-        testCase "get_share2" test_get_share2,
-        testCase "get_share3" test_get_share3,
+        testCase "test_empty_dividend_calculation" test_empty_dividend_calculation, 
+        testCase "test_dividend_calculation" test_dividend_calculation,
+        testCase "test_parseHolding" test_parseHolding,
+        testCase "test_getShareName_from_csvLine_1" test_getShareName_from_csvLine_1,
+        testCase "test_getShareName_from_csvLine_2" test_getShareName_from_csvLine_2,
+        testCase "test_getShareName_from_csvLine_3" test_getShareName_from_csvLine_3,
         testCase "test_parse_transaction" test_parse_transaction,
         testCase "test_parse_dividend" test_parse_dividend
         ]

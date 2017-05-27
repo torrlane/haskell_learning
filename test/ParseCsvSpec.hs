755ca8ca95984547ce933c5ded79f58e9c593f4f
John Robinson <github@torrlane.co.uk>
2017-05-27 09:47:40 +0100

    renamed tests to camelCase

--- test/ParseCsvSpec.hs
+++ test/ParseCsvSpec.hs
@@ -17,20 +17,21 @@ import ParseCsv                             (getShareName)
 
 parseCsvTests :: Test
 parseCsvTests = testGroup "parseCsvTests" [
-        testCase "test_empty_dividend_calculation" test_empty_dividend_calculation, 
-        testCase "test_dividend_calculation" test_dividend_calculation,
-        testCase "test_parseHolding" test_parseHolding,
-        testCase "test_create_holdings" test_create_holdings
+        testCase "testEmptyDividendCalculation" testEmptyDividendCalculation, 
+        testCase "testDividendCalculation" testDividendCalculation,
+        testCase "testParseHolding" testParseHolding,
+        testCase "testCreateHoldings" testCreateHoldings
         ]
         
+-- testParseShareHolding
 {-
  - test dividends_paid_upto returns 0 when no dividends are supplied
  -}
-test_empty_dividend_calculation :: Assertion
-test_empty_dividend_calculation = assertEqual "" 0 $ A.dividends_paid_upto undefined [] []
+testEmptyDividendCalculation :: Assertion
+testEmptyDividendCalculation = assertEqual "" 0 $ A.dividends_paid_upto undefined [] []
 
-test_dividend_calculation :: Assertion
-test_dividend_calculation = 
+testDividendCalculation :: Assertion
+testDividendCalculation = 
     let purchaseDate = epoch
         dividendPaymentDate = epoch
         transaction = T.Transaction{T.actioned_on=purchaseDate,T.shares_bought=8,T.cost=0}
@@ -41,8 +42,8 @@ test_dividend_calculation =
 
 
 
-test_parseHolding :: Assertion
-test_parseHolding = 
+testParseHolding :: Assertion
+testParseHolding = 
     let 
         expected = A.Holding{A.share="TSCO", A.transactions=[], A.dividends=[]} 
     in
@@ -51,8 +52,8 @@ test_parseHolding =
 
 
 
-test_create_holdings :: Assertion
-test_create_holdings =
+testCreateHoldings :: Assertion
+testCreateHoldings =
     -- transaction with no dividends
     let transactionDate1 = fromGregorian 2016 12 07
         transaction1 = T.Transaction{T.actioned_on=transactionDate1, T.shares_bought=1, T.cost=11} 

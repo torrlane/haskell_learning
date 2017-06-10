2ec116398df78a43017af42d695e044312201722
John Robinson <github@torrlane.co.uk>
2017-06-10 21:06:23 +0100

    Added shell script to run hlint over whole project

---
+++ bin/run_hlint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Run hlint over all haskell files in the project automatically applying suggestions
+# to install hlint run:
+# stack install hlint
+# stack install apply-refactor
+find . -type f -iname "*.hs" | xargs -I{} hlint {} --refactor --refactor-options=-i

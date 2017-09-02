aefee857f4541b45d3236cd406615debb5bb6a5f
osboxes.org <github@torrlane.co.uk>
2017-09-02 15:41:54 -0400

    added account question. used stylish haskell

--- app/ParseCsvMain.hs
+++ app/ParseCsvMain.hs
@@ -1,84 +1,97 @@
 module ParseCsvMain where
-import           Data.Char          (isSpace)
-import           Data.Csv           (FromRecord)
-import           Data.List          (concatMap, dropWhile, dropWhileEnd, length, lines)
-import           Data.Map           as M (Map, empty, fromList, findWithDefault, keys, union, foldrWithKey, toList)
-import           Hl.Csv.Dividend    (Dividend, amount, getDividends, paidOn)
-import           Hl.Csv.Transaction (Transaction(cost), actionedOn, getTransactions)
-import           Hl.Csv.AccountSummary (AccountSummary, ShareHolding, date, findShareHolding, getAccountSummaries, holdingValue)
-import           Lib                (createHoldings)
-import           ParseCsv           (getShareName)
-import           System.Directory   (getHomeDirectory, listDirectory)
-import           System.FilePath    (combine)
-import           System.IO          (BufferMode (LineBuffering),
-                                     IOMode (ReadMode), hClose, hGetContents,
-                                     hSetBuffering, openFile, putStrLn, stdout)
-import           Utils              (defaultWhenNull, listFilesInFolder,
-                                     stripWhitespace, toTwoDp)
+
+import Data.Char (isSpace)
+import Data.Csv (FromRecord)
+import Data.List
+       (concatMap, dropWhile, dropWhileEnd, length, lines)
+import Data.Map as M
+       (Map, empty, findWithDefault, foldrWithKey, fromList, keys, toList,
+        union)
+import Hl.Csv.AccountSummary
+       (AccountSummary, ShareHolding, date, findShareHolding,
+        getAccountSummaries, holdingValue)
+import Hl.Csv.Dividend (Dividend, amount, getDividends, paidOn)
+import Hl.Csv.Transaction
+       (Transaction(cost), actionedOn, getTransactions)
+import Lib (createHoldings)
+import ParseCsv (getShareName)
+import System.Directory (getHomeDirectory, listDirectory)
+import System.FilePath (combine)
+import System.IO
+       (BufferMode(LineBuffering), IOMode(ReadMode), hClose, hGetContents,
+        hSetBuffering, openFile, putStrLn, stdout)
+import Utils
+       (defaultWhenNull, listFilesInFolder, stripWhitespace, toTwoDp)
 
 main :: IO ()
 main = do
--- set stdout to use linebuffering so that get/print IO actions work as expected
-    hSetBuffering stdout LineBuffering
-    home <- getHomeDirectory
-    let defaultDividendFolder = home ++ "/Downloads/Dividends/"
-    let dividendPrompt = "Pleade provide a dividends folder"
-    let defaultTransactionFolder = home ++ "/Downloads/Transactions/"
-    let transactionPrompt = "Please provide a transactions folder"
-    let defaultAccountSummaryFolder = home ++ "/Downloads/AccountSummary"
-    let accountSummaryPrompt = "Please provide an accountSummary folder"
-
-    dividendsFolder <- questionWithDefault dividendPrompt defaultDividendFolder
-    dividendsMap <- getDividends dividendsFolder
-    putStrLn $ showShareMap dividendsMap
-
-    transactionsFolder <- questionWithDefault transactionPrompt defaultTransactionFolder
-    transactionsMap <- getTransactions transactionsFolder
-    putStrLn $ showShareMap transactionsMap
-
-    accountSummaryFolder <- questionWithDefault accountSummaryPrompt defaultAccountSummaryFolder
-    accountSummaries <- getAccountSummaries accountSummaryFolder
-    putStrLn $ concatMap ((++"\n\n") . show) accountSummaries
-
-    let shareTransactions = M.toList transactionsMap
-    let divs = \s -> M.findWithDefault [] s dividendsMap
-    let shareTransactionDividends = map (\(s, ts) -> (s, ts, divs s)) shareTransactions
-
-    let as = head accountSummaries
-    putStrLn "Share\t\t\t\t\t\t\tPriceProfit\tdividendProfit\ttotal"
-    mapM_ putStrLn $ map (showProfit as) shareTransactionDividends
+  hSetBuffering stdout LineBuffering
+  home <- getHomeDirectory
+  let defaultAccountName = "isa"
+  let accountPrompt = "Please provide an account name"
+  accountName <- questionWithDefault accountPrompt defaultAccountName
+  let baseFolder = home ++ "/Downloads/" ++ accountName
+  let defaultDividendFolder = baseFolder ++ "/Dividends/"
+  let dividendPrompt = "Pleade provide a dividends folder"
+  dividendsFolder <- questionWithDefault dividendPrompt defaultDividendFolder
+  dividendsMap <- getDividends dividendsFolder
+  putStrLn $ showShareMap dividendsMap
+  let defaultTransactionFolder = baseFolder ++ "/Transactions/"
+  let transactionPrompt = "Please provide a transactions folder"
+  transactionsFolder <-
+    questionWithDefault transactionPrompt defaultTransactionFolder
+  transactionsMap <- getTransactions transactionsFolder
+  putStrLn $ showShareMap transactionsMap
+  let defaultAccountSummaryFolder = baseFolder ++ "/AccountSummary"
+  let accountSummaryPrompt = "Please provide an accountSummary folder"
+  accountSummaryFolder <-
+    questionWithDefault accountSummaryPrompt defaultAccountSummaryFolder
+  accountSummaries <- getAccountSummaries accountSummaryFolder
+  putStrLn $ concatMap ((++ "\n\n") . show) accountSummaries
+  let shareTransactions = M.toList transactionsMap
+  let divs = \s -> M.findWithDefault [] s dividendsMap
+  let shareTransactionDividends =
+        map (\(s, ts) -> (s, ts, divs s)) shareTransactions
+  let as = head accountSummaries
+  putStrLn "Share\t\t\t\t\t\t\tPriceProfit\tdividendProfit\ttotal"
+  mapM_ (putStrLn . showProfit as) shareTransactionDividends
     --putStrLn $ concatMap ((++"\n\n") . show) shareTransactionDividends
 
-
+-- set stdout to use linebuffering so that get/print IO actions work as expected
 showProfit :: AccountSummary -> (String, [Transaction], [Dividend]) -> String
 showProfit as (s, [], ds) = "\n"
-showProfit as (s, (t:ts), ds) = 
-    let mshareHolding = findShareHolding s as
-    in
-    case mshareHolding of Nothing -> "\n"
-                          Just sh -> s ++ "\t" ++ (showR (priceProfit sh)) ++ "\t\t" ++ (showR dividendProfit) ++ "\t\t" ++ (showR (totalProfit sh) )
-    where dividendProfit = (transactionDividendProfit t as ds)
-          priceProfit sh = (transactionPriceProfit t sh)
-          totalProfit sh = dividendProfit + (priceProfit sh)
-          showR d = show (toTwoDp d)
-                          
-
-    
+showProfit as (s, t:ts, ds) =
+  let mshareHolding = findShareHolding s as
+  in case mshareHolding of
+       Nothing -> "\n"
+       Just sh ->
+         s ++
+         "\t" ++
+         showR (priceProfit sh) ++
+         "\t\t" ++ showR dividendProfit ++ "\t\t" ++ showR (totalProfit sh)
+  where
+    dividendProfit = transactionDividendProfit t as ds
+    priceProfit sh = transactionPriceProfit t sh
+    totalProfit sh = dividendProfit + priceProfit sh
+    showR d = show (toTwoDp d)
 
 -- | Calculate the profit from the transaction based purely on the share price change
 transactionPriceProfit :: Transaction -> ShareHolding -> Double
-transactionPriceProfit t s = (holdingValue s) - (cost t)
+transactionPriceProfit t s = holdingValue s - cost t
 
 -- | Calculate the profit from the Dividends for the transaction.
-transactionDividendProfit :: Transaction -> AccountSummary -> [Dividend] -> Double
+transactionDividendProfit ::
+     Transaction -> AccountSummary -> [Dividend] -> Double
 transactionDividendProfit t as ds = sum $ filter inDateRange ds
-    where sum = foldl (\v d -> v + (amount d)) 0
-          inDateRange = (\d -> (paidOn d) > (actionedOn t) && (paidOn d) < (date as))
+  where
+    sum = foldl (\v d -> v + amount d) 0
+    inDateRange = (\d -> (paidOn d) > (actionedOn t) && (paidOn d) < (date as))
+
 {-
  - for each transaction, get all the dividends for that transaction
- - then get the accountSummary/shareHOlding for the share - the name in the transaction is a prefix of the share name in the accountSummary 
+ - then get the accountSummary/shareHOlding for the share - the name in the transaction is a prefix of the share name in the accountSummary
  - then using the shareholding and the transaction, calculate the gain/loss of the shareprice
- - then using the dividends and the date of the transaction/shareholding, calculate the dividends paid 
+ - then using the dividends and the date of the transaction/shareholding, calculate the dividends paid
  - accountSummaries provide share values i.e. how much a share is worth at a particular point in time.
  - calculate the total amount of dividends paid for a transaction
  -
@@ -86,13 +99,13 @@ transactionDividendProfit t as ds = sum $ filter inDateRange ds
  - sharePriceProfit :: Transaction -> AccountSummary -> amount
  - calculate the total profit on the shareprice for a transaction (using the latest accountSummary to find the end price)
  -}
-
 -- | returns a string representation of a map from Share to an array of type 'a'
 showShareMap :: (Show a) => M.Map String [a] -> String
 showShareMap as = foldrWithKey entryToString "" as
-    where entryToString share as acc = acc ++ (shareTitle share) ++ (showValues as)  
-          shareTitle share = "values for " ++ share ++ "\n"
-          showValues as = concatMap ((++"\n") . show) as
+  where
+    entryToString share as acc = acc ++ shareTitle share ++ showValues as
+    shareTitle share = "values for " ++ share ++ "\n"
+    showValues as = concatMap ((++ "\n") . show) as
 
 {-
  - Takes a question to ask the user i.e "please provide a folder", and a default value.
@@ -100,8 +113,7 @@ showShareMap as = foldrWithKey entryToString "" as
  -}
 questionWithDefault :: String -> String -> IO String
 questionWithDefault question dfault = do
-    putStrLn $ question ++ " [" ++ dfault ++ "]"
-    input <- getLine
-    let answer = stripWhitespace $ defaultWhenNull dfault input
-    return answer
-
+  putStrLn $ question ++ " [" ++ dfault ++ "]"
+  input <- getLine
+  let answer = stripWhitespace $ defaultWhenNull dfault input
+  return answer
--- src/ParseCsv.hs
+++ src/ParseCsv.hs
@@ -1,29 +1,30 @@
 module ParseCsv
-    (
-    buildMap, getShareName, decodeCsv
-    )
-where
-import           Control.Monad           (mzero)
-import qualified Data.ByteString.Lazy    as B (ByteString)
-import           Data.Csv                (FromRecord (parseRecord),
-                                          HasHeader (NoHeader), Parser, Record,
-                                          decode, (.!))
-import           Data.Either.Combinators (fromRight)
-import           Data.List               (drop, isPrefixOf, null, stripPrefix,
-                                          takeWhile)
-import           Data.Map                as M (Map, empty, union)
-import           Data.Time.Calendar      (Day)
-import           Data.Time.Format        (defaultTimeLocale, parseTimeOrError)
-import           Data.Vector             (Vector, toList)
-import           Data.Vector             as V (empty)
-import           Utils                   (parseDate, stripWhitespace, toFiveDp,
-                                          toLazyByteString)
+  ( buildMap
+  , getShareName
+  , decodeCsv
+  ) where
+
+import Control.Monad (mzero)
+import qualified Data.ByteString.Lazy as B (ByteString)
+import Data.Csv
+       (FromRecord(parseRecord), HasHeader(NoHeader), Parser, Record,
+        (.!), decode)
+import Data.Either.Combinators (fromRight)
+import Data.List (drop, isPrefixOf, null, stripPrefix, takeWhile)
+import Data.Map as M (Map, empty, union)
+import Data.Time.Calendar (Day)
+import Data.Time.Format (defaultTimeLocale, parseTimeOrError)
+import Data.Vector (Vector, toList)
+import Data.Vector as V (empty)
+import Utils
+       (parseDate, stripWhitespace, toFiveDp, toLazyByteString)
 
 getShareName :: String -> Maybe String
 getShareName s
-    | null (shareName s) = Nothing
-    | otherwise     = Just $ shareName s
-    where shareName  = stripWhitespace . takeWhile (','/=) . tail . dropWhile (','/=)
+  | null (shareName s) = Nothing
+  | otherwise = Just $ shareName s
+  where
+    shareName = stripWhitespace . takeWhile (',' /=) . tail . dropWhile (',' /=)
 
 {- Removes the header section from the transactions csv file, returning just the csv lines -}
 stripHeader :: Int -> String -> String
@@ -32,18 +33,23 @@ stripHeader h = unlines . drop h . lines
 stripFooter :: String -> String
 stripFooter = unlines . takeWhile (not . isPrefixOf "\"Totals\"") . lines
 
-{- Takes a parser function and a list of files and produces a map from the share name to the lists of the parsed values
+{- Takes a parser function and a list of files and produces a map from the share name to the lists of the parsed values
  -}
-buildMap :: (FromRecord a) => (FilePath -> IO (M.Map String [a])) -> [FilePath] -> IO (M.Map String [a])
+buildMap ::
+     (FromRecord a)
+  => (FilePath -> IO (M.Map String [a]))
+  -> [FilePath]
+  -> IO (M.Map String [a])
 buildMap parseFile = foldl acc (return M.empty)
-    where acc ioMap f = do
-            newMap <- parseFile f
-            accMap <- ioMap
-            return $ union accMap newMap
+  where
+    acc ioMap f = do
+      newMap <- parseFile f
+      accMap <- ioMap
+      return $ union accMap newMap
 
-{-
- - Takes a String, removes any header lines, and then parses the remaining lines into instances of Type a
+{-
+ - Takes a String, removes any header lines, and then parses the remaining lines into instances of Type a
  -}
 decodeCsv :: FromRecord a => Int -> String -> Either String (Vector a)
-decodeCsv h str = decode NoHeader $ toLazyByteString . stripHeader h . stripFooter $ str
-
+decodeCsv h str =
+  decode NoHeader $ toLazyByteString . stripHeader h . stripFooter $ str

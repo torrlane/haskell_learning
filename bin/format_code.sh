f9b152175a932195c325c604bb24f8517e0082b2
osboxes.org <github@torrlane.co.uk>
2018-03-08 17:39:11 -0500

    updated install instructions for bin files

--- bin/format_code.sh
+++ bin/format_code.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # format the code according to some popular haskell formatting conventions
-# stack install stylish-haskell
-find . -type f -iname "*.hs" | xargs -I{} stylish-haskell -i {} 
+# stack build --copy-compiler-tool stylish-haskell
+ghc_verion=8.2.2
+bin_location=/home/osboxes/.stack/compiler-tools/x86_64-linux/ghc-${ghc_version}/bin
+stylish_haskell_location=`stack exec -- which stylish-haskell ${bin_location}`
+
+find . -name .stack-work -prune -o -type f -iname "*.hs" -print | xargs -I{} ${stylish_haskell_location} -i {} 
--- bin/run_hlint.sh
+++ bin/run_hlint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Run hlint over all haskell files in the project automatically applying suggestions
 # to install hlint run:
-# stack install hlint
-# stack install apply-refactor
-find . -type f -iname "*.hs" | xargs -I{} hlint {} --report --refactor --refactor-options=-i
-hlint . --report
+# stack build --copy-compiler-tool hlint
+# stack build --copy-compiler-tool apply-refact
+ghc_verion=8.2.2
+bin_location=/home/osboxes/.stack/compiler-tools/x86_64-linux/ghc-${ghc_version}/bin
+hlint_location=`stack exec -- which hlint ${bin_location}`
+find . -type f -iname "*.hs" | xargs -I{} ${hlint_location} {} --report --refactor --refactor-options=-i
+${hlint_location} . --report
